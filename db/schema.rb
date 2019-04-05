b3309c9cf13dd935a0661d128ba70ab86c9a4ad7
handaman <handaman.job@gmail.com>
2019-04-05 02:41:05 +0000

    create owner table2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190405012613) do
+ActiveRecord::Schema.define(version: 20190405023632) do
+
+  create_table "owners", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
