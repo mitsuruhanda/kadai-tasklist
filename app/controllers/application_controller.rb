5298095bb8b38b080f4131092d2b68664da98244
Mitsuru Handa <mitsuru.handa@kiramex.com>
2019-04-10 12:12:30 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    if Rails.application.secrets[:basic_auth_enable] == "true"
+    if Rails.application.secrets[:basic_auth_enable]
       authenticate_or_request_with_http_basic do |username, password|
        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
       end
